fdcba714921be747289fdc3ed0943723ff03572c
Ccindy0171 <58992827+Ccindy0171@users.noreply.github.com>
2022-01-27 14:04:20 -0500

    Create Gates.cpp

---
+++ Gates.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+const int MAX = 1e5 + 1;
+
+int G, P;
+set<int> ports;
+
+int main(){
+    int G, P;
+    cin >> G >> P;
+    
+    for (int i=1; i<=G; i++){
+        ports.insert(i);
+    }
+    
+    int ans = 0;
+    int g;
+    bool full = false;
+    for (int i=0; i<P; i++){
+        cin >> g;
+        if (!full){
+            auto x = ports.upper_bound(g);
+            if (x != ports.begin()){
+                x--;
+                ports.erase(x);
+                ans ++;
+            } else{
+                full = true;
+            }
+        }
+    }
+    
+    cout << ans;
+    
+    return 0;
+}
